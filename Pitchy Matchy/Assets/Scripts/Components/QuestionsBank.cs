1dd6f303f969d6d6484236e35a9ed7cf8465510f
hrkrdy <63145494+hszrdy@users.noreply.github.com>
2025-07-14 22:03:26 +0800

    Update QuestionBank.cs check desc
    added method for getting question in the question bank
    added private method for sorting the general question bank into their respective difficulties

--- Pitchy Matchy/Assets/Scripts/Components/QuestionsBank.cs
+++ Pitchy Matchy/Assets/Scripts/Components/QuestionsBank.cs
@@ -4,7 +4,74 @@ using UnityEngine;
 
 public class QuestionsBank : MonoBehaviour
 {
-    [SerializeField] public List<QuestionComponent> availableQuestionsPool;
+    [SerializeField] private List<QuestionComponent> availableQuestionsPool;
+    [SerializeField] private List<QuestionComponent> easyQuestionsPool;
+    [SerializeField] private List<QuestionComponent> mediumQuestionsPool;
+    [SerializeField] private List<QuestionComponent> hardQuestionsPool;
 
+    void Awake()
+    {
+        if (availableQuestionsPool == null) availableQuestionsPool = new List<QuestionComponent>();
+        if (easyQuestionsPool      == null) easyQuestionsPool      = new List<QuestionComponent>();
+        if (mediumQuestionsPool    == null) mediumQuestionsPool    = new List<QuestionComponent>();
+        if (hardQuestionsPool      == null) hardQuestionsPool      = new List<QuestionComponent>();
+        SortIntoDifficulties();
+    }
+
+    public QuestionComponent GetQuestionFromBank(QuestionComponent.DifficultyClass difficulty)
+    {
+        QuestionComponent question;
+        int n = 0;
+        switch (difficulty)
+        {
+            case QuestionComponent.DifficultyClass.EASY:
+                //random question from easy list
+                n = easyQuestionsPool.Count - 1;
+                question = new QuestionComponent(easyQuestionsPool[Random.Range(0, n)]);
+                break;
+
+            case QuestionComponent.DifficultyClass.MEDIUM:
+                //random question from medium list
+                n = mediumQuestionsPool.Count - 1;
+                question = new QuestionComponent(mediumQuestionsPool[Random.Range(0, n)]);
+                break;
+
+            case QuestionComponent.DifficultyClass.HARD:
+                //random question from hard list
+                n = hardQuestionsPool.Count - 1;
+                question = new QuestionComponent(hardQuestionsPool[Random.Range(0, n)]);
+                break;
+
+            default:
+                question = null;
+                break;
+        }
+
+        return question;
+    }
+
+    private void SortIntoDifficulties()
+    {
+        foreach (var question in availableQuestionsPool)
+        {
+            switch (question.questionDifficulty)
+            {
+                case QuestionComponent.DifficultyClass.EASY:
+                    easyQuestionsPool.Add(question);
+                    break;
+
+                case QuestionComponent.DifficultyClass.MEDIUM:
+                    mediumQuestionsPool.Add(question);
+                    break;
+
+                case QuestionComponent.DifficultyClass.HARD:
+                    hardQuestionsPool.Add(question);
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
     //add methods for retrieving blocks of questions with difficulty filters
 }
