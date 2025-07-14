f36022f28a93145cb9610209e1db5094a1df7a10
hrkrdy <63145494+hszrdy@users.noreply.github.com>
2025-07-14 22:04:59 +0800

    Update QuestionComponent.cs check desc
    added constructor for copy purposes (this is done due to copy-by-reference in object passing)
    added method for resetting the question (just means this question hasnt been answered yet)

--- Pitchy Matchy/Assets/Scripts/Components/QuestionComponent.cs
+++ Pitchy Matchy/Assets/Scripts/Components/QuestionComponent.cs
@@ -5,12 +5,40 @@ using UnityEngine;
 [System.Serializable]
 public class QuestionComponent
 {
-    public enum DifficultyClass {EASY, MEDIUM, HARD}
+    public enum DifficultyClass { EASY, MEDIUM, HARD }
     public string questionText;
     public DifficultyClass questionDifficulty;
     public List<AudioClip> soundClips;
     public List<string> correctAnswers;
     public List<string> playerAnswers;
-    public bool isAnsweredCorrectly;
+
+    //if player answered correctly or wrong
+    public bool isAnsweredCorrectly = false; 
+
+    //check if the question has been answered either wrong or correctly
+    public bool hasBeenAnswered = false;
+
+    public QuestionComponent()
+    {
+        
+    }
+
+    // CONSTRUCTOR FOR COPY-PURPOSES
+    public QuestionComponent(QuestionComponent other)
+    {
+        questionText = other.questionText;
+        questionDifficulty = other.questionDifficulty;
+        soundClips = new List<AudioClip>(other.soundClips);
+        correctAnswers = new List<string>(other.correctAnswers);
+        playerAnswers = new List<string>(other.playerAnswers);
+        isAnsweredCorrectly = other.isAnsweredCorrectly;
+    }
+
+    public void ResetQuestion()
+    {
+        playerAnswers.Clear();
+        hasBeenAnswered = false;
+        isAnsweredCorrectly = false;
+    }
 }
 
