ec7492f41442d486b125bc15743cdf8bacca70b8
hrkrdy <63145494+hszrdy@users.noreply.github.com>
2025-07-17 23:08:00 +0800

    added random question loader

--- Pitchy Matchy/Assets/Scripts/Components/QuizHandler.cs
+++ Pitchy Matchy/Assets/Scripts/Components/QuizHandler.cs
@@ -1,14 +1,26 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
-    //quiz handler will bridge the PianoHandler (player inputs), question answering and etc.
+//quiz handler will bridge the PianoHandler (player inputs), question answering and etc.
 public class QuizHandler : MonoBehaviour
 {
     [Header("Questions Loaded(visible for testing)")]
     [SerializeField] public List<QuestionComponent> questionsToAnswer; //will be dynamically filled up
 
+    [Header("Question Bank")]
+    [SerializeField] private QuestionsBank bank;
+    [Header("Number of Questions")]
+    [SerializeField] private int numberOfQuestions;
+
     private List<string> playerAnswers = new List<string>();
+
+    public void Start()
+    {
+        LoadRandomQuestions(numberOfQuestions);
+    }
 
     public void ReceivePlayerAnswers(List<string> answers)
     {
@@ -17,6 +29,20 @@ public class QuizHandler : MonoBehaviour
         foreach (var item in playerAnswers)
         {
             Debug.Log(item);
+        }
+    }
+
+    public void LoadRandomQuestions(int numberOfQuestions)
+    {
+
+        for (int i = 0; i < numberOfQuestions; i++)
+        {
+            QuestionComponent.DifficultyClass randDifficulty = (QuestionComponent.DifficultyClass) UnityEngine.Random.Range(
+                0,
+                System.Enum.GetValues(typeof(QuestionComponent.DifficultyClass)).Length - 1
+            );
+
+            questionsToAnswer.Add(bank.GetQuestionFromBank(randDifficulty));
         }
     }
 
