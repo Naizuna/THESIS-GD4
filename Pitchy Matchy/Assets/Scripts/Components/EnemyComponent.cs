147d8aad71e162dbc2e2cca8ef5d2c99780481f2
hrkrdy <63145494+hszrdy@users.noreply.github.com>
2025-08-16 00:52:41 +0800

    Update EnemyComponent.cs

--- Pitchy Matchy/Assets/Scripts/Components/EnemyComponent.cs
+++ Pitchy Matchy/Assets/Scripts/Components/EnemyComponent.cs
@@ -5,11 +5,14 @@ using UnityEngine.UI;
 
 public class EnemyComponent : MonoBehaviour
 {
-    [SerializeField] Sprite enemySprite;
+    [SerializeField] SpriteRenderer enemySprite;
+    [SerializeField] Color damagedColor;
+    [SerializeField] float damagedFlashDuration;
     [SerializeField] float enemyHP;
     [SerializeField] int attackPower;
     [SerializeField] Slider hpBar;
     private bool isDefeated;
+    private Color defaultColor;
     float maxhp;
     void Update()
     {
@@ -25,11 +28,15 @@ public class EnemyComponent : MonoBehaviour
     private void Start()
     {
         isDefeated = false;
+        defaultColor = enemySprite.color;
         maxhp = enemyHP;
     }
     public void TakeDamage(int damage)
     {
         Debug.Log($"Enemy takes {damage} damage!");
+
+        StartCoroutine(HurtFlash());
+
         if ((enemyHP - damage) < 0)
         {
             isDefeated = true;
@@ -39,6 +46,14 @@ public class EnemyComponent : MonoBehaviour
 
         enemyHP -= damage;
     }
+
+    private IEnumerator HurtFlash()
+    {
+        enemySprite.color = damagedColor;
+        yield return new WaitForSeconds(damagedFlashDuration);
+        enemySprite.color = defaultColor;
+    }
+
 
     public bool IsDefeated()
     {
